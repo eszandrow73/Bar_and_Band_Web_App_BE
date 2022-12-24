38c93a851cfbbcf8ece809e17b70f87c5c0dba09
eszandrow73 <63029414+eszandrow73@users.noreply.github.com>
2022-12-23 22:30:57 -0500

    added email route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const PORT = 8999
 //import Express from 'express'
 const express = require('express')
 const cors = require('cors')
+var nodemailer = require('nodemailer');
+
 /*
 const axios = require("axios")
 const cheerio = require("cheerio")
@@ -47,12 +49,16 @@ var connectionDB = makeConnect()
 //postgres://postgres:P@125362a@localhost:5432/testData
 //'SELECT * FROM public."testTable"')
 
+
+
 app.get('/', async(req , res ) => {
     //res.html(html_example)
     html_test = "<html><h1>Bar and Band API for Postgres Database</h1></html>"
     res.set('Content-Type', 'text/html');
     res.send(Buffer.from(html_test));
 })
+
+
 
 app.get('/barData', async(req, res)=>{
     
@@ -125,16 +131,14 @@ app.get('/bandData', async(req, res)=>{
 app.get('/addBand', async(req, res)=>{
     
     var inputList = req.query
-    console.log(inputList)
-    console.log(inputList.input)
-    console.log(inputList.input[1])
+
     var addToDB = ""
     addToDB = " VALUES(" + inputList.input[0] + ", '"
     addToDB = addToDB + inputList.input[1] + "', '"
     addToDB = addToDB + inputList.input[2] + "', '"
     addToDB = addToDB + inputList.input[3] + "', '"
     addToDB = addToDB + inputList.input[4] + "');"
-    console.log(addToDB)
+
     var outData = {}
    barClient.query('INSERT INTO public.banddata (id, spotify, website, "style", members)'+ addToDB)
     .then((dbRes)=>{
@@ -187,6 +191,89 @@ app.get('/test', async(req, res)=>{
     })
     
     res.json(outData)
+})
+
+app.get('/artData', async(req, res)=>{
+    
+    var outData = {}
+
+    
+   barClient.query('select * from public."artistTable"')
+    .then((dbRes)=>{
+        //console.log(res)
+        console.table(dbRes.rows)
+        console.log(dbRes.rows)
+        outData = dbRes.rows
+        res.json(outData)
+        
+    }).catch((e)=>{
+        console.log(e)
+        res.json(e)
+    })
+})
+
+app.get('/sendEmail', async(req, res)=>{
+    var nodemailer = require('nodemailer');
+
+    var transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "0ebdd5fb17a376",
+          pass: "3caedef039849c"
+        }
+      });
+
+    var mailOptions = {
+        from: 'ericzan73964@gmail.com',
+        to: 'ericzan73@aol.com',
+        subject: 'test email from api route',
+        text: 'That was easy!'
+    };
+
+    transport.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(info);
+            res.json({'email status': 'sent'})
+        }
+    });
+})
+
+
+app.get("/checkUser/:userName", async(req,res)=>{
+    console.log(req.params)
+    
+    barClient.query('SELECT username, "password", email FROM public.user_info '+ `WHERE username = '${req.params.userName}'`)
+    .then((dbRes)=>{
+        //console.log(res)
+        console.table(dbRes.rows) 
+        console.log(dbRes.rows)
+        outData = dbRes.rows
+    }).catch((e)=>{
+        console.log(e)
+        res.json(e)
+    }).then(async()=>{
+        res.json(outData)
+    })
+})
+
+app.get("/band/:bandnum/member/:mnum", async(req, res)=>{
+    //console.log(req.params.bandnum)
+    //console.log(req.params.mnum)
+    barClient.query(`SELECT * FROM public."bandMembers" where ("bandID"=${req.params.bandnum} AND "memberID"=${req.params.mnum})`)
+    .then((dbRes)=>{
+        //console.log(res)
+        //console.table(dbRes.rows) 
+        //console.log(dbRes.rows)
+        outData = dbRes.rows
+    }).catch((e)=>{
+        console.log(e)
+        res.json(e)
+    }).then(async()=>{
+        res.json(outData)
+    })
 })
 
 app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
